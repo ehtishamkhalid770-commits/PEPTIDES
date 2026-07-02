3143ce7d46a4df3300c4fa59e9f203e61fa08199
ehtishamkhalid770-commits <ehtisham.khalid770@gmail.com>
2026-07-03 04:30:14 +0500

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,4 @@
 import { Product } from "./types";
-import { retatrude } from "/src/assets/images/retatrutide_box_vial_1783031480387.jpg";
 
 
 export const PRODUCTS: Product[] = [
@@ -25,7 +24,7 @@ export const PRODUCTS: Product[] = [
       "Metabolic optimization",
       "Active individuals & athletes"
     ],
-    image:{retatrude},
+    image:"/src/assets/images/retatrutide_box_vial_1783031480387.jpg",
     specs: [
       { label: "Concentration", value: "30 mg" },
       { label: "Vial Size", value: "3 mL" },
